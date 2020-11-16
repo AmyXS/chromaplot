9f8a37ff21de4a55038796a450e2f63617ad1f4c
Amy Wang <amyshuangw@gmail.com>
2020-11-16 13:59:15 +1300

    Plot spectrogram for a mono file

--- chromaprint.py
+++ chromaprint.py
@@ -1,43 +1,41 @@
+#import the pyplot and wavfile modules 
+
 import matplotlib.pyplot as plot
-import numpy as np
 
-# Define the list of frequencies
-frequencies         = np.arange(5,105,5)
-
-# Sampling Frequency
-samplingFrequency   = 400
-
-
-# Create two ndarrays
-s1 = np.empty([0]) # For samples
-s2 = np.empty([0]) # For signal
-
-start   = 1
-stop    = samplingFrequency+1
-
-
-for frequency in frequencies:
-    sub1 = np.arange(start, stop, 1)
-    # Signal - Sine wave with varying frequency + Noise
-    sub2 = np.sin(2*np.pi*sub1*frequency*1/samplingFrequency)+np.random.randn(len(sub1))
-    s1      = np.append(s1, sub1)
-    s2      = np.append(s2, sub2) 
-
-    start   = stop+1
-    stop    = start+samplingFrequency
+from scipy.io import wavfile
 
  
-# Plot the signal
+
+# Read the wav file (mono)
+
+samplingFrequency, signalData = wavfile.read('1.wav')
+
+ 
+
+# Plot the signal read from wav file
+
 plot.subplot(211)
-plot.plot(s1,s2)
+
+plot.title('Spectrogram of a wav file with piano music')
+
+ 
+
+plot.plot(signalData)
+
 plot.xlabel('Sample')
+
 plot.ylabel('Amplitude')
 
+ 
 
-# Plot the spectrogram
 plot.subplot(212)
-powerSpectrum, freqenciesFound, time, imageAxis = plot.specgram(s2, Fs=samplingFrequency)
+
+plot.specgram(signalData,Fs=samplingFrequency)
+
 plot.xlabel('Time')
+
 plot.ylabel('Frequency')
 
-plot.show()   
+ 
+
+plot.show()
